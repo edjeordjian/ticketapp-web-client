c3d86d62683f149ee830d6faa3238510753160b8
Esteban <edjeordjian@fi.uba.ar>
2023-04-02 17:34:02 -0300

    Get types

---
+++ ticket-app/src/constants/URLs.js
@@ -0,0 +1,11 @@
+const SIGN_IN_URL = "/signin";
+
+const EVENT_URL = "/event";
+
+const EVENT_SEARCH_NAME_URL = "/event/all";
+
+const EVENT_TYPES_URL = EVENT_URL + "/types";
+
+export {
+    SIGN_IN_URL, EVENT_URL, EVENT_SEARCH_NAME_URL, EVENT_TYPES_URL
+};
